a24a1632b2df9551a8f9815d53fa3681c2efa1f1
Lam Haoyin <root@eatswap.org>
2022-01-01 00:58:24 +0800

    add: Happy 2022!

--- 2201/220101-CN.cpp
+++ 2201/220101-CN.cpp
@@ -1,4 +1,27 @@
-//
-// Created by Lam Haoyin on 01/01/22.
-//
+#include <vector>
+#include <cstdio>
 
+class Solution {
+public:
+	static std::vector<std::vector<int>> construct2DArray(std::vector<int>& original, int m, int n) {
+		if (m * n != original.size()) return {};
+		std::vector<std::vector<int>> ret;
+		ret.reserve(m);
+		for (int i = 0; i < m; ++i) {
+			ret.emplace_back(original.begin() + (i * n), original.begin() + ((1 + i) * n));
+		}
+		return ret;
+	}
+};
+
+int main() {
+	std::vector<int> s {1, 2};
+	const auto ret = Solution::construct2DArray(s, 1, 1);
+	for (const auto& i : ret) {
+		for (const auto& j : i) {
+			std::printf("%d ", j);
+		}
+		std::printf("\n");
+	}
+	return 0;
+}
