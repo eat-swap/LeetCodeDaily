7f1e9bbfa3cea3ef19feec51a230dc1ca5747e83
Lam Haoyin <root@eatswap.org>
2022-01-08 01:05:26 +0800

    feat: add description

--- 2201/220108-CN.cpp
+++ 2201/220108-CN.cpp
@@ -2,6 +2,17 @@
 #include <iostream>
 #include <chrono>
 
+/**
+ * 89. Gray Code
+ * An n-bit gray code sequence is a sequence of 2^n integers where:
+ * Every integer is in the inclusive range [0, 2^n - 1],
+ * The first integer is 0,
+ * An integer appears no more than once in the sequence,
+ * The binary representation of every pair of adjacent integers differs by exactly one bit, and
+ * The binary representation of the first and last integers differs by exactly one bit.
+ * Given an integer n, return any valid n-bit gray code sequence.
+ */
+
 class Solution {
 public:
 	static std::vector<int> grayCode(const int n) {
