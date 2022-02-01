8a7e8c00e8b37dd81de0a8f0bcc5761cebc31f57
Lam Haoyin <root@eatswap.org>
2022-02-01 22:38:36 +0800

    add: descriptions

--- 2201/220129.cpp
+++ 2201/220129.cpp
@@ -1,9 +1,13 @@
 #include <vector>
 #include <iostream>
 #include <random>
-#include <queue>
 #include <ctime>
 #include <chrono>
+
+/**
+ * 84. Largest Rectangle in Histogram
+ * Given an array of integers heights representing the histogram's bar height where the width of each bar is 1, return the area of the largest rectangle in the histogram.
+ */
 
 class Solution {
 private:
