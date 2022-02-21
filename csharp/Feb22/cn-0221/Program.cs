d7bb6d819cb93a24a2869c12c2921f505e059cb9
Lam Haoyin <root@eatswap.org>
2022-02-21 12:53:42 +0800

    feat: stash changes

---
+++ csharp/Feb22/cn-0221/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace cn_0221;
+
+public class Solution {
+	public static string PushDominoes(string dominoes) {
+		var q = new Queue<int>();
+
+		var n = dominoes.Length;
+		for (int i = 0; i < n; ++i) {
+			if ('.' != dominoes[i]) {
+				q.Enqueue(i);
+				q.Enqueue(0);
+			}
+		}
+
+		var result = new int[n];
+		var lastModified = new int[n];
+		while (q.Count > 0) {
+			var position = q.Dequeue();
+			var timePoint = q.Dequeue();
+			
+		}
+		
+		return "test";
+	}
+
+	public static void Main() {
+		// empty
+	}
+}
