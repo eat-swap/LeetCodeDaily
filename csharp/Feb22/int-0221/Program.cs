a8bd54d0d28e6e7e7556cef364cfa3e9c86bb50c
Lam Haoyin <root@eatswap.org>
2022-02-22 00:39:55 +0800

    add: 220221 [csharp]

---
+++ csharp/Feb22/int-0221/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace int_0221;
+
+public class Solution {
+	public int MajorityElement(int[] nums) {
+		var m = new Dictionary<int, int>();
+		var n = nums.Length >> 1;
+		foreach (var i in nums) {
+			if (m.ContainsKey(i))
+				++m[i];
+			else
+				m[i] = 1;
+			if (m[i] > n)
+				return i;
+		}
+		// Never reaches
+		return 0;
+	}
+}
+
+public class Runner {
+	private static void Main() {
+		// Tested in C++
+	}
+}
