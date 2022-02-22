ecb3d135df71c6174fae040d62eb66d239595c11
Lam Haoyin <root@eatswap.org>
2022-02-22 18:12:57 +0800

    add: 220222 [csharp]

---
+++ csharp/Feb22/int-0222/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace int_0222;
+
+public class Solution {
+	public int TitleToNumber(string columnTitle) {
+		int ret = 0;
+		foreach (char c in columnTitle)
+			ret = (26 * ret) - 'A' + c + 1;
+		return ret;
+	}
+}
+
+public class Runner {
+	public static void Main() {
+		var s = new Solution();
+		Console.WriteLine(s.TitleToNumber("ZY"));
+	}
+}
