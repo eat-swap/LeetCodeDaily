395e0c060b4c848d61a35f1df800279f32c28ae2
Lam Haoyin <root@eatswap.org>
2022-02-24 18:44:05 +0800

    add: descriptions

--- cpp/2202/220223-CN.cpp
+++ cpp/2202/220223-CN.cpp
@@ -2,6 +2,14 @@
 #include <cctype>
 #include <iostream>
 
+/**
+ * 917. Reverse Only Letters
+ * Given a string s, reverse the string according to the following rules:
+ * All the characters that are not English letters remain in the same position.
+ * All the English letters (lowercase or uppercase) should be reversed.
+ * Return s after reversing it.
+ */
+
 class Solution {
 public:
 	static std::string reverseOnlyLetters(std::string s) {
