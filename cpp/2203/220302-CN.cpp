c19b1159047f66e24d7cdcd5c5053e9dcd84a041
Lam Haoyin <root@eatswap.org>
2022-03-02 22:25:33 +0800

    add: description

--- cpp/2203/220302-CN.cpp
+++ cpp/2203/220302-CN.cpp
@@ -1,6 +1,12 @@
 #include <string>
 #include <iostream>
 
+/**
+ * 564. Find the Closest Palindrome
+ * Given a string n representing an integer, return the closest integer (not including itself), which is a palindrome. If there is a tie, return the smaller one.
+ * The closest is defined as the absolute difference minimized between two integers.
+ */
+
 class Solution {
 private:
 	static inline constexpr unsigned long long diffULL(unsigned long long x, unsigned long long y) {
