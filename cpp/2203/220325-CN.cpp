8de2cc8b259d38b11cdfb0d43af281f8835cf393
Lam Haoyin <root@eatswap.org>
2022-03-25 19:34:34 +0800

    add: 220325-CN [cpp]

---
+++ cpp/2203/220325-CN.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+class Solution {
+public:
+	// This algorithm is far NOT optimal.
+	static constexpr int trailingZeroes(int n) {
+		// Main algorithm: 2 * 5 = 10.
+		// Time complexity: O(n * sqrt(n))
+		int cnt2 = 0, cnt5 = 0;
+		for (int i = 2; i <= n; ++i) {
+			for (int x = i; !(x % 2); x /= 2)
+				++cnt2;
+			for (int x = i; !(x % 5); x /= 5)
+				++cnt5;
+		}
+		return std::min(cnt2, cnt5);
+	}
+};
+
+int main() {
+	std::cout << Solution::trailingZeroes(5);
+	return 0;
+}
