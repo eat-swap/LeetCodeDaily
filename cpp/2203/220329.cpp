7ffcac9824f6f90662ebc6bbb6a8412feea822b7
Lam Haoyin <root@eatswap.org>
2022-03-29 22:47:13 +0800

    add: desc [cpp]

--- cpp/2203/220329.cpp
+++ cpp/2203/220329.cpp
@@ -1,6 +1,13 @@
 #include <vector>
 #include <bitset>
 
+/**
+ * 287. Find the Duplicate Number
+ * Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
+ * There is only one repeated number in nums, return this repeated number.
+ * You must solve the problem without modifying the array nums and uses only constant extra space.
+ */
+
 class Solution {
 public:
 	static int findDuplicate(const std::vector<int>& nums) {
