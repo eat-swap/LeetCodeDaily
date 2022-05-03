1b2bcc45d882eb94b9bb6b53c2b3d4c7e9b17e32
Eat-Swap <root@eatswap.org>
2022-05-03 13:07:32 +0800

    add: 220502 [cpp]

---
+++ cpp/2205/220502.cpp
@@ -0,0 +1,7 @@
+/**
+ * 905. Sort Array By Parity
+ * Given an integer array nums, move all the even integers at the beginning of the array followed by all the odd integers.
+ * Return any array that satisfies this condition.
+ *
+ * Refer: 220428-CN.cpp
+ */
