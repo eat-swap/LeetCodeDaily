290b6e1d861b0e3a409da7a8873f6c8a74e0aaab
Eat-Swap <root@eatswap.org>
2022-06-05 02:12:51 +0800

    add: 220603 [cpp]

---
+++ cpp/2206/220603.cpp
@@ -0,0 +1,10 @@
+/**
+ * 304. Range Sum Query 2D - Immutable
+ * Given a 2D matrix matrix, handle multiple queries of the following type:
+ * Calculate the sum of the elements of matrix inside the rectangle defined by its upper left corner (row1, col1) and lower right corner (row2, col2).
+ * Implement the NumMatrix class:
+ * NumMatrix(int[][] matrix) Initializes the object with the integer matrix matrix.
+ * int sumRegion(int row1, int col1, int row2, int col2) Returns the sum of the elements of matrix inside the rectangle defined by its upper left corner (row1, col1) and lower right corner (row2, col2).
+ *
+ * Refer: more/0304.cpp
+ */
