5341fdda8f36424b243a2ed02e4d3808d9924bda
Eatswap <root@eatswap.org>
2022-10-24 12:22:28 +0800

    add: desc

--- cpp/2210/221024-CN.cpp
+++ cpp/2210/221024-CN.cpp
@@ -1,6 +1,17 @@
 #include <vector>
 #include <iostream>
 
+/**
+ * 915. Partition Array into Disjoint Intervals
+ *
+ * Given an integer array nums, partition it into two (contiguous) subarrays left and right so that:
+ * - Every element in left is less than or equal to every element in right.
+ * - left and right are non-empty.
+ * - left has the smallest possible size.
+ * Return the length of left after such a partitioning.
+ * Test cases are generated such that partitioning exists.
+ */
+
 class Solution {
 private:
 	static int partitionDisjoint_1(const std::vector<int>& nums) {
