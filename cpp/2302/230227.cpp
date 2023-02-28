2cc35c392a5623c362d7a4e3a248154d9f6a6664
Eatswap <root@eatswap.org>
2023-02-28 20:41:42 +0800

    add: 230227

---
+++ cpp/2302/230227.cpp
@@ -0,0 +1,7 @@
+/**
+ * 427. Construct Quad Tree
+ * Given a n * n matrix grid of 0's and 1's only. We want to represent the grid with a Quad-Tree.
+ * Return the root of the Quad-Tree representing the grid.
+ *
+ * Refer: 220429-CN.cpp
+ */
